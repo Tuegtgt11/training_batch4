dfbe1069180ccf8a936451486c6cfc80b1cc29b7
Vu Duc Tue <tuevdth2010017@fpt.edu.vn>
2022-11-12 12:18:53 +0700

    update bai

--- demobuoi1/src/Main.java
+++ demobuoi1/src/Main.java
@@ -1,12 +1,46 @@
+import inheritance.SubClass;
+import inheritance.SuperClass;
+import interface_demo.ClassUsingInterface;
+
 public class Main {
-     Integer a = 200;
-     Integer b = Integer.parseInt("200");
-     Integer c = 2000;
-     Integer d = 2000;
+    public final static int finalInt = 10; // const
 
-     boolean SS1 = a == b;
-     boolean SS2 = c == d;
     public static void main(String[] args) {
+        inheritanceDemo();
+        System.out.println("=====================");
+        interfaceDemo(20, 100);
+        System.out.println("=====================");
+        Integer a = 2000;
+        Integer b = 2000;
+        boolean c = a == b;
 
+        System.out.println(c);
+
+    }
+
+    private static void inheritanceDemo() {
+        SubClass subClass = new SubClass();
+        SuperClass superClass = new SuperClass();
+        System.out.println("Super Class: ");
+        System.out.println("Ten: " + superClass.getName());
+        System.out.println("Tuoi: " + superClass.getAge());
+        System.out.println("Dia chi: " + superClass.getAddress());
+
+        System.out.println("=====================");
+
+        System.out.println("Sub Class: ");
+        System.out.println("Ten: " + subClass.getName());
+        System.out.println("Tuoi: " + subClass.getAge());
+        System.out.println("Dia chi duoc thua huong tu super class: " + subClass.getAddress());
+    }
+
+    private static void interfaceDemo(Integer a, int b) {
+        ClassUsingInterface classUsingInterface = new ClassUsingInterface();
+        System.out.println("a + b = " + classUsingInterface.sum(a, b));
+        System.out.println("a - b = " + classUsingInterface.minus(a, b));
+    }
+
+    static {
+        int staticInt = 100;
     }
 }
---
+++ demobuoi1/src/inheritance/SuperClass.java
@@ -0,0 +1,32 @@
+package inheritance;
+
+public class SuperClass {
+    private String name = "Nguyen Van ABC";
+    private int age = 20;
+    private String address = "Vietnam";
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
