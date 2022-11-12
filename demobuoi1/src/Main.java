0e8d77a2110552885a9dd74edb21be7d474a2e24
Vu Duc Tue <tuevdth2010017@fpt.edu.vn>
2022-11-12 11:18:30 +0700

    first commit

---
+++ demobuoi1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+     Integer a = 200;
+     Integer b = Integer.parseInt("200");
+     Integer c = 2000;
+     Integer d = 2000;
+
+     boolean SS1 = a == b;
+     boolean SS2 = c == d;
+    public static void main(String[] args) {
+
+    }
+}
